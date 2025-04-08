a2b8b596b861d574e634cd2a7087c9e01ae64422
Rouvik Maji <majirouvik@gmail.com>
2025-04-09 02:03:52 +0530

    Update databases

---
+++ server/DBHandler.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+class MongooseConnect {
+    static isConnected = false;
+
+    static async connect(URI, force = false) {
+        if (force) {
+            MongooseConnect.isConnected = false;
+        }
+
+        if (!MongooseConnect.isConnected) {
+            await mongoose.connect(URI);
+            MongooseConnect.isConnected = true;
+            console.log("[MongooseConnect INFO] Successfully connected to MongoDB!");
+        }
+    }
+}
+
+class UserDB {
+    static userSchema = new mongoose.Schema({
+        userId: { type: String, required: true, unique: true },
+        name: { type: String, required: true },
+        enrolledCourses: {
+            type: [
+                {
+                    name: { type: String, required: true },
+                    progress: { type: Number, required: true, default: 0 }
+                }
+            ], default: []
+        },
+        completedCourses: {
+            type: [String],
+            default: []
+        }
+    });
+
+    static Users = mongoose.model('users', UserDB.userSchema);
+
+    async middleware_userAuth(req, res, next) {
+        if (!req.session.accountedFor) {
+            req.session.accountedFor = true;
+
+            try {
+                const userDbStore = await UserDBHandler.Users.findOne({
+                    userID: req.auth.userId
+                });
+
+                if (userDbStore === null) {
+                    const userData = await clerk.clerkClient.users.getUser(req.auth.userId);
+                    const userStore = new UserDBHandler.Users({
+                        userId: req.auth.userId,
+                        name: userData.username,
+                        enrolledCourses: [],
+                        completedCourses: []
+                    });
+
+                    await userStore.save();
+                }
+            } catch (error) {
+                console.log("[UserDBHandler Error] userAuthMiddleWare failed to authenticate user", error);
+            }
+        }
+
+        next();
+    }
+}
+
+module.exports = {
+    MongooseConnect,
+    UserDB
+};
--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+// base modules -------------------
+const crypto = require('crypto');
+const session = require('express-session');
+
+// custom modules ----------------
+const { MongooseConnect, UserDB } = require('./DBHandler.js');
+
 // express ----------------------------
 const express = require('express');
 const app = express();
@@ -7,9 +14,20 @@ const dotenv = require('dotenv');
 dotenv.config();
 process.env.PORT = process.env.PORT || '8080';
 
+// session handler ----------------
+app.use(session({
+    secret: crypto.randomBytes(32),
+    maxAge: 24 * 60 * 60 * 1000     // 24 hour expiry
+}));
+
 // clerk ------------------------------
 const clerk = require('@clerk/express');
 app.use(clerk.clerkMiddleware());
+
+// database connection --------------
+MongooseConnect.connect(process.env.MONGO_URI);
+
+const userDBHandler = new UserDB();
 
 // public server -------------------
 app.use('/public', express.static('client/public'));
@@ -19,7 +37,9 @@ app.get('/', (req, res) => {
 });
 
 // private server ------------------
-app.use('/private', clerk.requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL, signUpUrl: process.env.CLERK_SIGN_UP_URL }), express.static('client/private'));
+app.use('/private', clerk.requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL, signUpUrl: process.env.CLERK_SIGN_UP_URL }),
+    userDBHandler.middleware_userAuth.bind(userDBHandler),
+    express.static('client/private'));
 
 
 app.listen(+process.env.PORT, () => {
