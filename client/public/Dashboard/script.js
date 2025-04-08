cd3117a2e8fcf007d06f251546932ef1e6dc7a26
Archis <archismanpaul09@outlook.com>
2025-04-09 02:46:54 +0530

    Temporary dashboard

---
+++ client/public/Dashboard/script.js
@@ -0,0 +1,27 @@
+const courses = [
+  {
+    title: "Naya bharat ka naya kishaan",
+    instructor: "Harish",
+    progress: 30,
+    thumbnail: "https://source.unsplash.com/400x300/?agriculture,farmer"
+  },
+  {
+    title: "Hello Kishan",
+    instructor: "Suresh",
+    progress: 65,
+    thumbnail: "https://source.unsplash.com/400x300"
+  },
+  {
+    title: "krishi darshan",
+    instructor: "Ramesh",
+    progress: 90,
+    thumbnail: "https://source.unsplash.com/400x300"
+  },
+  {
+    title: "Humari dharti",
+    instructor: "Archis",
+    progress: 45,
+    thumbnail: "https://source.unsplash.com/400x300"
+  }
+];
+const dashboard = document.getElementById('dashboard');
